68aeac4471fe814aff46e40e7f78ec8e04f84350
Pierre-Olivier Mercier <nemunaire@nemunai.re>
2020-03-08 19:35:44 +0100

    Home page change when connected

--- htdocs/src/router/index.js
+++ htdocs/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +7,9 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: function () {
+      return import(/* webpackChunkName: "Index" */ '@/views/Index.vue')
+    }
   },
   {
     path: '/login',
---
+++ htdocs/src/views/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <component :is="homeComponent"></component>
+</template>
+
+<script>
+import Home from '@/views/Home'
+import ZoneList from '@/views/zone-list'
+
+export default {
+  data () {
+    return {
+      homeComponent: sessionStorage.token !== undefined ? ZoneList : Home
+    }
+  }
+}
+</script>
