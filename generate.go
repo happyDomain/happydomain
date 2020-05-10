1e8d6ad63f85f2835da8e36e5df58017120faaca
Pierre-Olivier Mercier <nemunaire@nemunai.re>
2020-05-11 00:12:08 +0200

    Generalize the icon generation

--- generate.go
+++ generate.go
@@ -32,4 +32,5 @@
 package main
 
 //go:generate go run generators/gen_database_migration.go
-//go:generate go run generators/gen_sources_icon.go
+//go:generate go run generators/gen_icon.go sources sources
+//go:generate go run generators/gen_icon.go services svcs
