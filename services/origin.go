8e5e0baf610d752faba156d874aca526eaca2052
Pierre-Olivier Mercier <nemunaire@nemunai.re>
2020-06-27 15:14:52 +0200

    Include NS records in Origin

--- services/origin.go
+++ services/origin.go
@@ -42,21 +42,27 @@ import (
 )
 
 type Origin struct {
-	Ns      string        `json:"mname" happydns:"label=Name Server,placeholder=ns0,required,description=The domain name of the name server that was the original or primary source of data for this zone."`
-	Mbox    string        `json:"rname" happydns:"label=Contact Email,required,description=A <domain-name> which specifies the mailbox of the person responsible for this zone."`
-	Serial  uint32        `json:"serial" happydns:"label=Zone Serial,required,description=The unsigned 32 bit version number of the original copy of the zone.  Zone transfers preserve this value.  This value wraps and should be compared using sequence space arithmetic."`
-	Refresh time.Duration `json:"refresh" happydns:"label=Slave Refresh Time,required,description=The time interval before the zone should be refreshed by others name servers than the primary."`
-	Retry   time.Duration `json:"retry" happydns:"label=Retry Interval on failed refresh,required,description=The time interval a slave name server should elapse before a failed refresh should be retried."`
-	Expire  time.Duration `json:"expire" happydns:"label=Authoritative Expiry,required,description=Time value that specifies the upper limit on the time interval that can elapse before the zone is no longer authoritative."`
-	Negttl  time.Duration `json:"nxttl" happydns:"label=Negative Caching Time,required,description=Maximal time a resolver should cache a negative authoritative answer (such as NXDOMAIN ...)."`
+	Ns          string        `json:"mname" happydns:"label=Name Server,placeholder=ns0,required,description=The domain name of the name server that was the original or primary source of data for this zone."`
+	Mbox        string        `json:"rname" happydns:"label=Contact Email,required,description=A <domain-name> which specifies the mailbox of the person responsible for this zone."`
+	Serial      uint32        `json:"serial" happydns:"label=Zone Serial,required,description=The unsigned 32 bit version number of the original copy of the zone.  Zone transfers preserve this value.  This value wraps and should be compared using sequence space arithmetic."`
+	Refresh     time.Duration `json:"refresh" happydns:"label=Slave Refresh Time,required,description=The time interval before the zone should be refreshed by others name servers than the primary."`
+	Retry       time.Duration `json:"retry" happydns:"label=Retry Interval on failed refresh,required,description=The time interval a slave name server should elapse before a failed refresh should be retried."`
+	Expire      time.Duration `json:"expire" happydns:"label=Authoritative Expiry,required,description=Time value that specifies the upper limit on the time interval that can elapse before the zone is no longer authoritative."`
+	Negttl      time.Duration `json:"nxttl" happydns:"label=Negative Caching Time,required,description=Maximal time a resolver should cache a negative authoritative answer (such as NXDOMAIN ...)."`
+	NameServers []string      `json:"ns" happydns:"label=Zone's Name Servers"`
 }
 
 func (s *Origin) GetNbResources() int {
-	return 1
+	return len(s.NameServers)
 }
 
 func (s *Origin) GenComment(origin string) string {
-	return fmt.Sprintf("%s %s %d", strings.TrimSuffix(s.Ns, "."+origin), strings.TrimSuffix(s.Mbox, "."+origin), s.Serial)
+	ns := ""
+	if s.NameServers != nil {
+		ns = fmt.Sprintf(" + %d NS", len(s.NameServers))
+	}
+
+	return fmt.Sprintf("%s %s %d"+ns, strings.TrimSuffix(s.Ns, "."+origin), strings.TrimSuffix(s.Mbox, "."+origin), s.Serial)
 }
 
 func (s *Origin) GenRRs(domain string, ttl uint32, origin string) (rrs []dns.RR) {
@@ -83,25 +89,53 @@ func (s *Origin) GenRRs(domain string, ttl uint32, origin string) (rrs []dns.RR)
 		Expire:  uint32(s.Expire.Seconds()),
 		Minttl:  uint32(s.Negttl.Seconds()),
 	})
+	for _, ns := range s.NameServers {
+		if ns[len(ns)-1] != '.' {
+			ns += origin
+		}
+
+		rrs = append(rrs, &dns.NS{
+			Hdr: dns.RR_Header{
+				Name:   domain,
+				Rrtype: dns.TypeNS,
+				Class:  dns.ClassINET,
+				Ttl:    ttl,
+			},
+			Ns: ns,
+		})
+	}
 	return
 }
 
 func origin_analyze(a *Analyzer) error {
 	for _, record := range a.searchRR(AnalyzerRecordFilter{Type: dns.TypeSOA}) {
 		if soa, ok := record.(*dns.SOA); ok {
+			origin := &Origin{
+				Ns:      soa.Ns,
+				Mbox:    soa.Mbox,
+				Serial:  soa.Serial,
+				Refresh: time.Duration(soa.Refresh) * time.Second,
+				Retry:   time.Duration(soa.Retry) * time.Second,
+				Expire:  time.Duration(soa.Expire) * time.Second,
+				Negttl:  time.Duration(soa.Minttl) * time.Second,
+			}
+
 			a.useRR(
 				record,
 				soa.Header().Name,
-				&Origin{
-					Ns:      soa.Ns,
-					Mbox:    soa.Mbox,
-					Serial:  soa.Serial,
-					Refresh: time.Duration(soa.Refresh) * time.Second,
-					Retry:   time.Duration(soa.Retry) * time.Second,
-					Expire:  time.Duration(soa.Expire) * time.Second,
-					Negttl:  time.Duration(soa.Minttl) * time.Second,
-				},
+				origin,
 			)
+
+			for _, record := range a.searchRR(AnalyzerRecordFilter{Type: dns.TypeNS, Domain: soa.Header().Name}) {
+				if ns, ok := record.(*dns.NS); ok {
+					origin.NameServers = append(origin.NameServers, ns.Ns)
+					a.useRR(
+						record,
+						ns.Header().Name,
+						origin,
+					)
+				}
+			}
 		}
 	}
 	return nil
