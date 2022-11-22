b52a25f90c96d0c9f6f571f1fbeff6d8dd892a19
Pierre-Olivier Mercier <nemunaire@nemunai.re>
2022-11-22 11:51:48 +0100

    Restyle vox-people button

--- ui/src/lib/components/VoxPeople.svelte
+++ ui/src/lib/components/VoxPeople.svelte
@@ -3,21 +3,40 @@
      Icon,
  } from 'sveltestrap';
 
+ const { MODE } = import.meta.env;
+
+ import { userSession } from '$lib/stores/usersession';
  import { t, locale } from '$lib/translations';
 
- const user_isLogged = false;
  const instancename = "";
  const router = {history:{current:{}}};
 </script>
 
-<a
-    id="voxpeople"
-    href="https://framaforms.org/quel-est-votre-avis-sur-happydns-1610366701?u={user_isLogged?user_getSession.id:0}&amp;i={instancename}&amp;p={router.history.current.name}&amp;l={$locale}"
-    target="_blank"
-    title={$t('common.survey')}
-    class="d-flex justify-content-center align-items-center umami--click--vox-people"
->
-    <Icon
-        name="chat-right-text"
-    />
-</a>
+{#if MODE == 'production'}
+    <a
+        id="voxpeople"
+        href="https://framaforms.org/quel-est-votre-avis-sur-happydns-1610366701?u={userSession?userSession.id:0}&amp;i={instancename}&amp;p={router.history.current.name}&amp;l={$locale}"
+        target="_blank"
+        rel="noreferrer"
+        title={$t('common.survey')}
+        class="d-flex btn btn-light justify-content-center align-items-center umami--click--vox-people"
+    >
+        <Icon
+            name="chat-right-text"
+        />
+    </a>
+{/if}
+
+<style>
+ #voxpeople {
+     position: fixed;
+     bottom: 7vh;
+     right: 4vw;
+     z-index: 1051;
+     height: max(4vw, 4vh);
+     width: max(4vw, 4vh);
+     border-radius: 4vw;
+     box-shadow: 0 0px 3px 0 #9332bb;
+     font-size: max(1.7vw, 1.7vh);
+ }
+</style>
