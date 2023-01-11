c75147c5ebe9a2324fc6c8227f0abd7589b19c42
Pierre-Olivier Mercier <nemunaire@nemunai.re>
2023-01-11 11:01:08 +0100

    ui: Consider net.IP as strings

--- ui/src/lib/types.ts
+++ ui/src/lib/types.ts
@@ -1,9 +1,12 @@
 import type { Field } from '$lib/model/custom_form';
 
 export function fillUndefinedValues(value: any, spec: Field) {
-    if (value[spec.id] === undefined) {
-        if (spec.type == "[]uint8") value[spec.id] = "";
-        else if (spec.type.startsWith("[]")) value[spec.id] = [];
-        else if (spec.type != "string" && !spec.type.startsWith("uint") && !spec.type.startsWith("int") && !spec.type.startsWith("time.Duration")) value[spec.id] = { };
+    if (value[spec.id] === undefined && spec.type.length) {
+        let vartype = spec.type;
+        if (vartype[0] == "*") vartype = vartype.substring(1);
+
+        if (vartype == "[]uint8") value[spec.id] = "";
+        else if (vartype.startsWith("[]")) value[spec.id] = [];
+        else if (vartype != "string" && !vartype.startsWith("uint") && !vartype.startsWith("int") && vartype != "time.Duration" && vartype != "net.IP") value[spec.id] = { };
     }
 }
