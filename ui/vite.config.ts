6367d5cfdb54ec26300508dfd174b938c98a61fb
Pierre-Olivier Mercier <nemunaire@nemunai.re>
2023-09-19 11:10:04 +0200

    ui: Fix HMR

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,6 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
+        server: {
+            hmr: {
+                port: 10000
+            }
+        },
+
 	plugins: [sveltekit()]
 };
 
