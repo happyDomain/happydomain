18cfc8f7751af0f8244845187755fd32ee06fa7c
David <devnull@daviddernoncourt.com>
2023-12-16 23:11:01 +0100

    New provider from DNSControl: OpenSRS

---
+++ providers/opensrs.go
@@ -0,0 +1,67 @@
+// Copyright or © or Copr. happyDNS (2021)
+//
+// contact@happydomain.org
+//
+// This software is a computer program whose purpose is to provide a modern
+// interface to interact with DNS systems.
+//
+// This software is governed by the CeCILL license under French law and abiding
+// by the rules of distribution of free software.  You can use, modify and/or
+// redistribute the software under the terms of the CeCILL license as
+// circulated by CEA, CNRS and INRIA at the following URL
+// "http://www.cecill.info".
+//
+// As a counterpart to the access to the provider code and rights to copy, modify
+// and redistribute granted by the license, users are provided only with a
+// limited warranty and the software's author, the holder of the economic
+// rights, and the successive licensors have only limited liability.
+//
+// In this respect, the user's attention is drawn to the risks associated with
+// loading, using, modifying and/or developing or reproducing the software by
+// the user in light of its specific status of free software, that may mean
+// that it is complicated to manipulate, and that also therefore means that it
+// is reserved for developers and experienced professionals having in-depth
+// computer knowledge. Users are therefore encouraged to load and test the
+// software's suitability as regards their requirements in conditions enabling
+// the security of their systems and/or data to be ensured and, more generally,
+// to use and operate it in the same conditions as regards security.
+//
+// The fact that you are presently reading this means that you have had
+// knowledge of the CeCILL license and that you accept its terms.
+
+package providers // import "git.happydns.org/happyDomain/providers"
+
+import (
+	"github.com/StackExchange/dnscontrol/v4/providers"
+	_ "github.com/StackExchange/dnscontrol/v4/providers/opensrs"
+
+	"git.happydns.org/happyDomain/model"
+)
+
+type OpensrsAPI struct {
+	ApiKey string `json:"apiKey,omitempty" happydomain:"label=API key,placeholder=xxxxxxxx,required,description=Your API key."`
+	Username string `json:"username,omitempty" happydomain:"label=Username,placeholder=xxxxxxxx,required,description=Your username."`
+	BaseUrl string `json:"base_url,omitempty" happydomain:"label=Base URL,placeholder=xxxxxxxx,description=Alternate base URL."`
+}
+
+func (s *OpensrsAPI) NewDNSServiceProvider() (providers.DNSServiceProvider, error) {
+	config := map[string]string{
+		"apikey": s.ApiKey,
+		"username": s.Username,
+		"baseurl": s.BaseUrl,
+	}
+	return providers.CreateDNSProvider(s.DNSControlName(), config, nil)
+}
+
+func (s *OpensrsAPI) DNSControlName() string {
+	return "OPENSRS"
+}
+
+func init() {
+	RegisterProvider(func() happydns.Provider {
+		return &OpensrsAPI{}
+	}, ProviderInfos{
+		Name:        "OpenSRS",
+		Description: "Domain registrar platform and wholesale domain services provider based in Toronto, Canada, and part of Tucows.",
+	})
+}
