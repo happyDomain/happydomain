2c222921415ab53b72f3cbff8b4d9d61e432bed0
Pierre-Olivier Mercier <nemunaire@nemunai.re>
2026-01-12 15:11:18 +0700

    Refactor ServiceEditor to use dynamic imports

--- web/src/lib/components/services/ServiceEditor.svelte
+++ web/src/lib/components/services/ServiceEditor.svelte
@@ -22,37 +22,8 @@
 -->
 
 <script lang="ts">
-    import ACMEChallengeEditor from '$lib/components/services/editors/abstract.ACMEChallenge.svelte'
-    import DelegationEditor from '$lib/components/services/editors/abstract.Delegation.svelte'
-    import GithubOrgVerifEditor from '$lib/components/services/editors/abstract.GithubOrgVerif.svelte'
-    import GitlabPageVerifEditor from '$lib/components/services/editors/abstract.GitlabPageVerif.svelte'
-    import GoogleVerifEditor from '$lib/components/services/editors/abstract.GoogleVerif.svelte'
-    import KeybaseVerifEditor from '$lib/components/services/editors/abstract.KeybaseVerif.svelte'
-    import MatrixIMEditor from '$lib/components/services/editors/abstract.MatrixIM.svelte'
-    import NsOnlyOriginEditor from '$lib/components/services/editors/abstract.NSOnlyOrigin.svelte'
-    import OpenPGPEditor from '$lib/components/services/editors/abstract.OpenPGP.svelte'
-    import OriginEditor from '$lib/components/services/editors/abstract.Origin.svelte'
-    import RFC6186Editor from '$lib/components/services/editors/abstract.RFC6186.svelte'
-    import ScalewayChallengeEditor from '$lib/components/services/editors/abstract.ScalewayChallenge.svelte'
-    import ServerEditor from '$lib/components/services/editors/abstract.Server.svelte'
-    import SMimeCertEditor from '$lib/components/services/editors/abstract.SMimeCert.svelte'
-    import XMPPEditor from '$lib/components/services/editors/abstract.XMPP.svelte'
-    import CAAEditor from '$lib/components/services/editors/svcs.CAAPolicy.svelte'
-    import CNAMEEditor from '$lib/components/services/editors/svcs.CNAME.svelte'
-    import DKIMEditor from '$lib/components/services/editors/svcs.DKIMRecord.svelte'
-    import DMARCEditor from '$lib/components/services/editors/svcs.DMARC.svelte'
-    import MTA_STSEditor from '$lib/components/services/editors/svcs.MTA_STS.svelte'
-    import MXsEditor from '$lib/components/services/editors/svcs.MXs.svelte'
-    import NAPTREditor from '$lib/components/services/editors/svcs.NAPTR.svelte'
-    import OrphanEditor from '$lib/components/services/editors/svcs.Orphan.svelte'
-    import PTREditor from '$lib/components/services/editors/svcs.PTR.svelte'
-    import SPFEditor from '$lib/components/services/editors/svcs.SPF.svelte'
-    import SSHFPsEditor from '$lib/components/services/editors/svcs.SSHFPs.svelte'
-    import TLSAsEditor from '$lib/components/services/editors/svcs.TLSAs.svelte'
-    import TLS_RPTEditor from '$lib/components/services/editors/svcs.TLS_RPT.svelte'
-    import UnknownSRVEditor from '$lib/components/services/editors/svcs.UnknownSRV.svelte'
-    import GSuiteEditor from '$lib/components/services/editors/providers.google.GSuite.svelte'
     import type { Domain } from "$lib/model/domain";
+    import OrphanEditor from '$lib/components/services/editors/svcs.Orphan.svelte';
 
     interface Props {
         dn: string;
@@ -62,71 +33,44 @@
     }
 
     let { dn, origin, type, value = $bindable({}) }: Props = $props();
+
+    // Map of all editor modules (lazy loaded)
+    const editorModules = import.meta.glob('./editors/*.svelte');
+
+    // Dynamically load the appropriate editor component
+    let componentPromise = $derived(
+        (async () => {
+            const filename = `${type}.svelte`;
+            const path = `./editors/${filename}`;
+
+            if (editorModules[path]) {
+                const module = await editorModules[path]() as { default: any };
+                return module.default;
+            }
+
+            // Fallback to Orphan editor for unknown types
+            return OrphanEditor;
+        })()
+    );
 </script>
 
-{#if type == "abstract.ACMEChallenge"}
-    <ACMEChallengeEditor {dn} {origin} bind:value={value} />
-{:else if type == "abstract.Delegation"}
-    <DelegationEditor {dn} {origin} bind:value={value} />
-{:else if type == "abstract.GithubOrgVerif"}
-    <GithubOrgVerifEditor {dn} {origin} bind:value={value} />
-{:else if type == "abstract.GitlabPageVerif"}
-    <GitlabPageVerifEditor {dn} {origin} bind:value={value} />
-{:else if type == "abstract.GoogleVerif"}
-    <GoogleVerifEditor {dn} {origin} bind:value={value} />
-{:else if type == "abstract.KeybaseVerif"}
-    <KeybaseVerifEditor {dn} {origin} bind:value={value} />
-{:else if type == "abstract.MatrixIM"}
-    <MatrixIMEditor {dn} {origin} bind:value={value} />
-{:else if type == "abstract.NSOnlyOrigin"}
-    <NsOnlyOriginEditor {dn} {origin} bind:value={value} />
-{:else if type == "abstract.OpenPGP"}
-    <OpenPGPEditor {dn} {origin} bind:value={value} />
-{:else if type == "abstract.Origin"}
-    <OriginEditor {dn} {origin} bind:value={value} />
-{:else if type == "abstract.RFC6186"}
-    <RFC6186Editor {dn} {origin} bind:value={value} />
-{:else if type == "abstract.ScalewayChallenge"}
-    <ScalewayChallengeEditor {dn} {origin} bind:value={value} />
-{:else if type == "abstract.Server"}
-    <ServerEditor {dn} {origin} bind:value={value} />
-{:else if type == "abstract.SMimeCert"}
-    <SMimeCertEditor {dn} {origin} bind:value={value} />
-{:else if type == "abstract.XMPP"}
-    <XMPPEditor {dn} {origin} bind:value={value} />
-{:else if type == "svcs.CAAPolicy"}
-    <CAAEditor {dn} {origin} bind:value={value} />
-{:else if type == "svcs.CNAME"}
-    <CNAMEEditor {dn} {origin} bind:value={value} />
-{:else if type == "svcs.DKIMRecord"}
-    <DKIMEditor {dn} {origin} bind:value={value} />
-{:else if type == "svcs.DMARC"}
-    <DMARCEditor {dn} {origin} bind:value={value} />
-{:else if type == "svcs.MTA_STS"}
-    <MTA_STSEditor {dn} {origin} bind:value={value} />
-{:else if type == "svcs.MXs"}
-    <MXsEditor {dn} {origin} bind:value={value} />
-{:else if type == "svcs.NAPTR"}
-    <NAPTREditor {dn} {origin} bind:value={value} />
-{:else if type == "svcs.PTR"}
-    <PTREditor {dn} {origin} bind:value={value} />
-{:else if type == "svcs.SPF"}
-    <SPFEditor {dn} {origin} bind:value={value} />
-{:else if type == "svcs.SSHFPs"}
-    <SSHFPsEditor {dn} {origin} bind:value={value} />
-{:else if type == "svcs.TLSAs"}
-    <TLSAsEditor {dn} {origin} bind:value={value} />
-{:else if type == "svcs.TLS_RPT"}
-    <TLS_RPTEditor {dn} {origin} bind:value={value} />
-{:else if type == "svcs.UnknownSRV"}
-    <UnknownSRVEditor {dn} {origin} bind:value={value} />
-{:else if type == "google.GSuite"}
-    <GSuiteEditor {dn} {origin} bind:value={value} />
-{:else}
-    <OrphanEditor
+{#await componentPromise}
+    <div class="text-center p-3">
+        <div class="spinner-border spinner-border-sm text-primary" role="status">
+            <span class="visually-hidden">Loading editor...</span>
+        </div>
+    </div>
+{:then EditorComponent}
+    <EditorComponent
         {dn}
         {origin}
         {type}
         bind:value={value}
     />
-{/if}
+{:catch error}
+    <div class="alert alert-warning">
+        <p>Failed to load editor for type: {type}</p>
+        <p class="small text-muted">Error: {error.message}</p>
+    </div>
+    <OrphanEditor {dn} {origin} {type} bind:value={value} />
+{/await}
