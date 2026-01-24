74a7e8396175885b8b16d978adb33e9651da1baa
Pierre-Olivier Mercier <nemunaire@nemunai.re>
2026-01-24 11:13:02 +0800

    web-admin: Implement checkers interface with option editor

---
+++ web/src/lib/components/checkers/CheckerOptionsGroups.svelte
@@ -0,0 +1,84 @@
+<!--
+     This file is part of the happyDomain (R) project.
+     Copyright (c) 2022-2026 happyDomain
+     Authors: Pierre-Olivier Mercier, et al.
+
+     This program is offered under a commercial and under the AGPL license.
+     For commercial licensing, contact us at <contact@happydomain.org>.
+
+     For AGPL licensing:
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU Affero General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU Affero General Public License for more details.
+
+     You should have received a copy of the GNU Affero General Public License
+     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
+
+<script lang="ts">
+    import { Card, CardBody, CardHeader } from "@sveltestrap/sveltestrap";
+
+    interface OptionDef {
+        id?: string;
+        label?: string;
+        type?: string;
+        default?: unknown;
+        placeholder?: string;
+        description?: string;
+        required?: boolean;
+    }
+
+    interface OptionGroup {
+        label: string;
+        opts: OptionDef[];
+    }
+
+    interface Props {
+        groups: OptionGroup[];
+    }
+
+    let { groups }: Props = $props();
+</script>
+
+{#each groups as optGroup}
+    {#if optGroup.opts.length > 0}
+        <Card class="mb-3">
+            <CardHeader>
+                <strong>{optGroup.label}</strong>
+                <small class="text-muted ms-2">(Read-only)</small>
+            </CardHeader>
+            <CardBody>
+                <dl class="row mb-0">
+                    {#each optGroup.opts as optDoc}
+                        {@const optName = optDoc.id!}
+                        <dt class="col-sm-4">
+                            {optDoc.label || optDoc.id}:
+                        </dt>
+                        <dd class="col-sm-8">
+                            {#if optDoc.default}
+                                <span class="text-muted d-block">{optDoc.default}</span>
+                            {:else if optDoc.placeholder}
+                                <em class="text-muted d-block">{optDoc.placeholder}</em>
+                            {/if}
+                            {#if optDoc.description}
+                                <small class="text-muted d-block">{optDoc.description}</small>
+                            {/if}
+                            <small class="text-muted">
+                                Type: {optDoc.type || "string"}
+                            </small>
+                            {#if optDoc.required}
+                                <small class="text-danger ms-2"> Required </small>
+                            {/if}
+                        </dd>
+                    {/each}
+                </dl>
+            </CardBody>
+        </Card>
+    {/if}
+{/each}
