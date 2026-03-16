happyDomain/happydomain

commit b2b6467575c5e551e76f72205469b3f185291fe2

Refactor domain/provider/zone usecase: split in multiple files

internal/usecase/zone_service/action_on_zone.go
@@ -1,0 +1,80 @@
+// This file is part of the happyDomain (R) project.
+// Copyright (c) 2020-2025 happyDomain
+// Authors: Pierre-Olivier Mercier, et al.
+//
+// This program is offered under a commercial and under the AGPL license.
+// For commercial licensing, contact us at <contact@happydomain.org>.
+//
+// For AGPL licensing:
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package zoneService
+
+import (
+	"fmt"
+
+	domainUC "git.happydns.org/happyDomain/internal/usecase/domain"
+	zoneUC "git.happydns.org/happyDomain/internal/usecase/zone"
+	"git.happydns.org/happyDomain/model"
+)
+
+type ActionOnDomainUsecase struct {
+	domainUpdater *domainUC.UpdateDomainUsecase
+	zoneCreator   *zoneUC.CreateZoneUsecase
+}
+
+func NewActionOnDomainUsecase(domainUpdater *domainUC.UpdateDomainUsecase, zoneCreator *zoneUC.CreateZoneUsecase) *ActionOnDomainUsecase {
+	return &ActionOnDomainUsecase{
+		domainUpdater: domainUpdater,
+		zoneCreator:   zoneCreator,
+	}
+}
+
+func (uc *ActionOnDomainUsecase) ActionOnEditableZone(user *happydns.User, domain *happydns.Domain, zone *happydns.Zone, act func(zone *happydns.Zone) error) (*happydns.Zone, error) {
+	var err error
+	newZone := zone
+
+	if zone.CommitDate != nil || zone.Published != nil {
+		// Create a new zone if the current one is in archived state
+		newZone = zone.DerivateNew()
+
+		err = uc.zoneCreator.Create(newZone)
+		if err != nil {
+			return nil, happydns.InternalError{
+				Err:         fmt.Errorf("unable to CreateZone in importZone: %s\n", err),
+				UserMessage: "Sorry, we are unable to create your zone.",
+			}
+		}
+
+		domain.ZoneHistory = append(
+			[]happydns.Identifier{newZone.Id}, domain.ZoneHistory...)
+
+		err = uc.domainUpdater.Update(domain.Id, user, func(dn *happydns.Domain) {
+			dn.ZoneHistory = domain.ZoneHistory
+		})
+		if err != nil {
+			return nil, happydns.InternalError{
+				Err:         fmt.Errorf("unable to UpdateDomain in importZone: %s\n", err),
+				UserMessage: "Sorry, we are unable to create your zone.",
+			}
+		}
+	}
+
+	err = act(newZone)
+	if err != nil {
+		return nil, err
+	}
+
+	return newZone, nil
+}

internal/usecase/zone_service/factory.go
@@ -1,0 +1,66 @@
+// This file is part of the happyDomain (R) project.
+// Copyright (c) 2020-2025 happyDomain
+// Authors: Pierre-Olivier Mercier, et al.
+//
+// This program is offered under a commercial and under the AGPL license.
+// For commercial licensing, contact us at <contact@happydomain.org>.
+//
+// For AGPL licensing:
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package zoneService
+
+import (
+	domainUC "git.happydns.org/happyDomain/internal/usecase/domain"
+	serviceUC "git.happydns.org/happyDomain/internal/usecase/service"
+	zoneUC "git.happydns.org/happyDomain/internal/usecase/zone"
+	"git.happydns.org/happyDomain/model"
+)
+
+type Service struct {
+	ActionOnDomainUC *ActionOnDomainUsecase
+	AddToZoneUC      *AddToZoneUsecase
+	DeleteFromZoneUC *DeleteFromZoneUsecase
+	UpdateServiceUC  *UpdateServiceUsecase
+}
+
+func NewZoneServiceUsecases(
+	domainUpdater *domainUC.UpdateDomainUsecase,
+	zoneCreator *zoneUC.CreateZoneUsecase,
+	validateService *serviceUC.ValidateServiceUsecase,
+	store serviceUC.ZoneUpdaterStorage,
+) *Service {
+	return &Service{
+		ActionOnDomainUC: NewActionOnDomainUsecase(domainUpdater, zoneCreator),
+		AddToZoneUC:      NewAddToZoneUsecase(store, validateService),
+		DeleteFromZoneUC: NewDeleteFromZoneUsecase(store),
+		UpdateServiceUC:  NewUpdateServiceUsecase(store),
+	}
+}
+
+func (s *Service) ActionOnEditableZone(user *happydns.User, domain *happydns.Domain, zone *happydns.Zone, act func(zone *happydns.Zone) error) (*happydns.Zone, error) {
+	return s.ActionOnDomainUC.ActionOnEditableZone(user, domain, zone, act)
+}
+
+func (s *Service) AddServiceToZone(user *happydns.User, domain *happydns.Domain, zone *happydns.Zone, subdomain happydns.Subdomain, origin happydns.Origin, service *happydns.Service) (*happydns.Zone, error) {
+	return zone, s.AddToZoneUC.AddService(zone, subdomain, origin, service)
+}
+
+func (s *Service) RemoveServiceFromZone(user *happydns.User, domain *happydns.Domain, zone *happydns.Zone, subdomain happydns.Subdomain, serviceID happydns.Identifier) (*happydns.Zone, error) {
+	return zone, s.DeleteFromZoneUC.DeleteService(zone, subdomain, serviceID)
+}
+
+func (s *Service) UpdateZoneService(user *happydns.User, domain *happydns.Domain, zone *happydns.Zone, subdomain happydns.Subdomain, serviceID happydns.Identifier, service *happydns.Service) (*happydns.Zone, error) {
+	return zone, s.UpdateServiceUC.Update(zone, subdomain, serviceID, service)
+}

internal/usecase/domain/factory.go
@@ -1,0 +1,83 @@
+// This file is part of the happyDomain (R) project.
+// Copyright (c) 2020-2025 happyDomain
+// Authors: Pierre-Olivier Mercier, et al.
+//
+// This program is offered under a commercial and under the AGPL license.
+// For commercial licensing, contact us at <contact@happydomain.org>.
+//
+// For AGPL licensing:
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package domain
+
+import (
+	domainLogUC "git.happydns.org/happyDomain/internal/usecase/domain_log"
+	providerUC "git.happydns.org/happyDomain/internal/usecase/provider"
+	zoneUC "git.happydns.org/happyDomain/internal/usecase/zone"
+	"git.happydns.org/happyDomain/model"
+)
+
+type Service struct {
+	CreateDomainUC *CreateDomainUsecase
+	DeleteDomainUC *DeleteDomainUsecase
+	GetDomainUC    *GetDomainUsecase
+	ListDomainsUC  *ListDomainsUsecase
+	UpdateDomainUC *UpdateDomainUsecase
+}
+
+func NewDomainUsecases(
+	store DomainStorage,
+	getProviderUC *providerUC.GetProviderUsecase,
+	getZoneUC *zoneUC.GetZoneUsecase,
+	domainExistenceUC *providerUC.DomainExistenceUsecase,
+	domainLogAppenderUC *domainLogUC.CreateDomainLogUsecase,
+) *Service {
+	getDomainUC := NewGetDomainUsecase(store, getZoneUC)
+
+	return &Service{
+		CreateDomainUC: NewCreateDomainUsecase(store, getProviderUC, domainExistenceUC, domainLogAppenderUC),
+		DeleteDomainUC: NewDeleteDomainUsecase(store),
+		GetDomainUC:    getDomainUC,
+		ListDomainsUC:  NewListDomainsUsecase(store),
+		UpdateDomainUC: NewUpdateDomainUsecase(store, getDomainUC, domainLogAppenderUC),
+	}
+}
+
+func (s *Service) CreateDomain(user *happydns.User, uz *happydns.Domain) error {
+	return s.CreateDomainUC.Create(user, uz)
+}
+
+func (s *Service) DeleteDomain(domainid happydns.Identifier) error {
+	return s.DeleteDomainUC.Delete(domainid)
+}
+
+func (s *Service) ExtendsDomainWithZoneMeta(domain *happydns.Domain) (*happydns.DomainWithZoneMetadata, error) {
+	return s.GetDomainUC.ExtendsDomainWithZoneMeta(domain)
+}
+
+func (s *Service) GetUserDomain(user *happydns.User, domainID happydns.Identifier) (*happydns.Domain, error) {
+	return s.GetDomainUC.ByID(user, domainID)
+}
+
+func (s *Service) GetUserDomainByFQDN(user *happydns.User, fqdn string) ([]*happydns.Domain, error) {
+	return s.GetDomainUC.ByFQDN(user, fqdn)
+}
+
+func (s *Service) ListUserDomains(user *happydns.User) ([]*happydns.Domain, error) {
+	return s.ListDomainsUC.List(user)
+}
+
+func (s *Service) UpdateDomain(domainID happydns.Identifier, user *happydns.User, updateFn func(*happydns.Domain)) error {
+	return s.UpdateDomainUC.Update(domainID, user, updateFn)
+}

internal/usecase/orchestrator/factory.go
@@ -1,0 +1,53 @@
+// This file is part of the happyDomain (R) project.
+// Copyright (c) 2020-2025 happyDomain
+// Authors: Pierre-Olivier Mercier, et al.
+//
+// This program is offered under a commercial and under the AGPL license.
+// For commercial licensing, contact us at <contact@happydomain.org>.
+//
+// For AGPL licensing:
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package orchestrator
+
+import (
+	domainUC "git.happydns.org/happyDomain/internal/usecase/domain"
+	domainlogUC "git.happydns.org/happyDomain/internal/usecase/domain_log"
+	providerUC "git.happydns.org/happyDomain/internal/usecase/provider"
+	zoneUC "git.happydns.org/happyDomain/internal/usecase/zone"
+)
+
+type Orchestrator struct {
+	RemoteZoneImporter    *RemoteZoneImporterUsecase
+	ZoneCorrectionApplier *ZoneCorrectionApplierUsecase
+	ZoneImporter          *ZoneImporterUsecase
+}
+
+func NewOrchestrator(
+	appendDomainLog *domainlogUC.CreateDomainLogUsecase,
+	domainUpdater *domainUC.UpdateDomainUsecase,
+	getProvider *providerUC.GetProviderUsecase,
+	listRecords *zoneUC.ListRecordsUsecase,
+	zoneCorrector *providerUC.ZoneCorrectorUsecase,
+	zoneCreator *zoneUC.CreateZoneUsecase,
+	zoneRetriever *providerUC.ZoneRetrieverUsecase,
+	zoneUpdater *zoneUC.UpdateZoneUsecase,
+) *Orchestrator {
+	zoneImporter := NewZoneImporterUsecase(domainUpdater, zoneCreator)
+	return &Orchestrator{
+		RemoteZoneImporter:    NewRemoteZoneImporterUsecase(appendDomainLog, getProvider, zoneImporter, zoneRetriever),
+		ZoneCorrectionApplier: NewZoneCorrectionApplierUsecase(appendDomainLog, domainUpdater, getProvider, listRecords, zoneCorrector, zoneCreator, zoneUpdater),
+		ZoneImporter:          zoneImporter,
+	}
+}

internal/usecase/provider/factory.go
@@ -1,0 +1,138 @@
+// This file is part of the happyDomain (R) project.
+// Copyright (c) 2020-2025 happyDomain
+// Authors: Pierre-Olivier Mercier, et al.
+//
+// This program is offered under a commercial and under the AGPL license.
+// For commercial licensing, contact us at <contact@happydomain.org>.
+//
+// For AGPL licensing:
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package provider
+
+import (
+	"git.happydns.org/happyDomain/model"
+)
+
+type Service struct {
+	CreateProviderUC  *CreateProviderUsecase
+	DeleteProviderUC  *DeleteProviderUsecase
+	UpdateProviderUC  *UpdateProviderUsecase
+	GetProviderUC     *GetProviderUsecase
+	ListProvidersUC   *ListProvidersUsecase
+	RetrieveZoneUC    *ZoneRetrieverUsecase
+	ZoneCorrectionsUC *ZoneCorrectorUsecase
+	DomainExistenceUC *DomainExistenceUsecase
+}
+
+func NewProviderUsecases(store ProviderStorage) *Service {
+	getProvider := NewGetProviderUsecase(store)
+	validator := &DefaultProviderValidator{}
+
+	return &Service{
+		CreateProviderUC:  NewCreateProviderUsecase(store, validator),
+		DeleteProviderUC:  NewDeleteProviderUsecase(store),
+		UpdateProviderUC:  NewUpdateProviderUsecase(store, getProvider, validator),
+		GetProviderUC:     getProvider,
+		ListProvidersUC:   NewListProvidersUsecase(store),
+		RetrieveZoneUC:    NewZoneRetrieverUsecase(),
+		ZoneCorrectionsUC: NewZoneCorrectorUsecase(),
+		DomainExistenceUC: NewDomainExistenceUsecase(),
+	}
+}
+
+func (s *Service) CreateProvider(user *happydns.User, msg *happydns.ProviderMessage) (*happydns.Provider, error) {
+	return s.CreateProviderUC.Create(user, msg)
+}
+
+func (s *Service) DeleteProvider(user *happydns.User, providerID happydns.Identifier) error {
+	return s.DeleteProviderUC.Delete(user, providerID)
+}
+
+func (s *Service) GetUserProvider(user *happydns.User, providerID happydns.Identifier) (*happydns.Provider, error) {
+	return s.GetProviderUC.Get(user, providerID)
+}
+
+func (s *Service) GetUserProviderMeta(user *happydns.User, providerID happydns.Identifier) (*happydns.ProviderMeta, error) {
+	return s.GetProviderUC.GetMeta(user, providerID)
+}
+
+func (s *Service) ListZoneCorrections(provider *happydns.Provider, domain *happydns.Domain, records []happydns.Record) ([]*happydns.Correction, error) {
+	return s.ZoneCorrectionsUC.List(provider, domain, records)
+}
+
+func (s *Service) ListUserProviders(user *happydns.User) ([]*happydns.ProviderMeta, error) {
+	return s.ListProvidersUC.List(user)
+}
+
+func (s *Service) RetrieveZone(provider *happydns.Provider, name string) ([]happydns.Record, error) {
+	return s.RetrieveZoneUC.RetrieveCurrentZone(provider, name)
+}
+
+func (s *Service) TestDomainExistence(provider *happydns.Provider, name string) error {
+	return s.DomainExistenceUC.TestDomainExistence(provider, name)
+}
+
+func (s *Service) UpdateProvider(providerID happydns.Identifier, user *happydns.User, updateFn func(*happydns.Provider)) error {
+	return s.UpdateProviderUC.Update(providerID, user, updateFn)
+}
+
+func (s *Service) UpdateProviderFromMessage(providerID happydns.Identifier, user *happydns.User, p *happydns.ProviderMessage) error {
+	return s.UpdateProviderUC.FromMessage(providerID, user, p)
+}
+
+type RestrictedService struct {
+	Service
+	config *happydns.Options
+}
+
+func NewRestrictedProviderUsecases(cfg *happydns.Options, store ProviderStorage) *RestrictedService {
+	s := NewProviderUsecases(store)
+	return &RestrictedService{
+		*s,
+		cfg,
+	}
+}
+
+func (s *RestrictedService) CreateProvider(user *happydns.User, msg *happydns.ProviderMessage) (*happydns.Provider, error) {
+	if s.config.DisableProviders {
+		return nil, happydns.ForbiddenError{Msg: "cannot add provider as DisableProviders parameter is set."}
+	}
+
+	return s.Service.CreateProvider(user, msg)
+}
+
+func (s *RestrictedService) DeleteProvider(user *happydns.User, providerID happydns.Identifier) error {
+	if s.config.DisableProviders {
+		return happydns.ForbiddenError{Msg: "cannot delete provider as DisableProviders parameter is set."}
+	}
+
+	return s.Service.DeleteProvider(user, providerID)
+}
+
+func (s *RestrictedService) UpdateProvider(providerID happydns.Identifier, user *happydns.User, updateFn func(*happydns.Provider)) error {
+	if s.config.DisableProviders {
+		return happydns.ForbiddenError{Msg: "cannot update provider as DisableProviders parameter is set."}
+	}
+
+	return s.Service.UpdateProvider(providerID, user, updateFn)
+}
+
+func (s *RestrictedService) UpdateProviderFromMessage(providerID happydns.Identifier, user *happydns.User, p *happydns.ProviderMessage) error {
+	if s.config.DisableProviders {
+		return happydns.ForbiddenError{Msg: "cannot update provider as DisableProviders parameter is set."}
+	}
+
+	return s.Service.UpdateProviderFromMessage(providerID, user, p)
+}

internal/usecase/service/factory.go
@@ -1,5 +1,5 @@
 // This file is part of the happyDomain (R) project.
-// Copyright (c) 2020-2024 happyDomain
+// Copyright (c) 2020-2025 happyDomain
 // Authors: Pierre-Olivier Mercier, et al.
 //
 // This program is offered under a commercial and under the AGPL license.
@@ -19,26 +19,28 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-package route
+package service
 
 import (
-	"github.com/gin-gonic/gin"
-
-	"git.happydns.org/happyDomain/internal/api-admin/controller"
-	"git.happydns.org/happyDomain/internal/api/middleware"
-	"git.happydns.org/happyDomain/internal/storage"
 	"git.happydns.org/happyDomain/model"
 )
 
-func declareZoneServiceRoutes(apiZonesRoutes *gin.RouterGroup, zc *controller.ZoneController, dependancies happydns.UsecaseDependancies, store storage.Storage) {
-	sc := controller.NewServiceController(
-		dependancies.ServiceUsecase(),
-		dependancies.ZoneServiceUsecase(),
-	)
+type Service struct {
+	GetRecordsUC      *GetRecordsUsecase
+	ValidateServiceUC *ValidateServiceUsecase
+}
 
-	apiZonesServiceIdRoutes := apiZonesRoutes.Group("/services/:serviceid")
-	apiZonesServiceIdRoutes.Use(middleware.ServiceIdHandler(dependancies.ServiceUsecase()))
-	apiZonesServiceIdRoutes.GET("", sc.GetZoneService)
-	apiZonesServiceIdRoutes.PUT("", sc.UpdateZoneService)
-	apiZonesServiceIdRoutes.DELETE("", sc.DeleteZoneService)
+func NewServiceUsecases() *Service {
+	return &Service{
+		GetRecordsUC:      NewGetRecordsUsecase(),
+		ValidateServiceUC: NewValidateServiceUsecase(),
+	}
+}
+
+func (s *Service) GetRecords(domain *happydns.Domain, zone *happydns.Zone, service *happydns.Service) ([]happydns.Record, error) {
+	return s.GetRecordsUC.List(domain, zone, service)
+}
+
+func (s *Service) ValidateService(body happydns.ServiceBody, subdomain happydns.Subdomain, origin happydns.Origin) ([]byte, error) {
+	return s.ValidateServiceUC.Validate(body, subdomain, origin)
 }

internal/usecase/zone/factory.go
@@ -1,0 +1,85 @@
+// This file is part of the happyDomain (R) project.
+// Copyright (c) 2020-2025 happyDomain
+// Authors: Pierre-Olivier Mercier, et al.
+//
+// This program is offered under a commercial and under the AGPL license.
+// For commercial licensing, contact us at <contact@happydomain.org>.
+//
+// For AGPL licensing:
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package zone
+
+import (
+	"git.happydns.org/happyDomain/model"
+)
+
+type Service struct {
+	CreateZoneUC  *CreateZoneUsecase
+	DeleteZoneUC  *DeleteZoneUsecase
+	DiffZoneUC    *ZoneDifferUsecase
+	GetZoneUC     *GetZoneUsecase
+	ListRecordsUC *ListRecordsUsecase
+	UpdateZoneUC  *UpdateZoneUsecase
+}
+
+func NewZoneUsecases(store ZoneStorage) *Service {
+	getZone := NewGetZoneUsecase(store)
+	listRecords := NewListRecordsUsecase()
+
+	return &Service{
+		CreateZoneUC:  NewCreateZoneUsecase(store),
+		DeleteZoneUC:  NewDeleteZoneUsecase(store),
+		DiffZoneUC:    NewZoneDifferUsecase(getZone, listRecords),
+		GetZoneUC:     getZone,
+		ListRecordsUC: listRecords,
+		UpdateZoneUC:  NewUpdateZoneUsease(store, getZone),
+	}
+}
+
+func (s *Service) CreateZone(zone *happydns.Zone) error {
+	return s.CreateZoneUC.Create(zone)
+}
+
+func (s *Service) DeleteZone(zoneid happydns.Identifier) error {
+	return s.DeleteZoneUC.Delete(zoneid)
+}
+
+func (s *Service) DiffZones(domain *happydns.Domain, newZone *happydns.Zone, oldZoneID happydns.Identifier) ([]*happydns.Correction, error) {
+	return s.DiffZoneUC.Diff(domain, newZone, oldZoneID)
+}
+
+func (s *Service) FlattenZoneFile(domain *happydns.Domain, zone *happydns.Zone) (string, error) {
+	return s.ListRecordsUC.ToZoneFile(domain, zone)
+}
+
+func (s *Service) GenerateRecords(domain *happydns.Domain, zone *happydns.Zone) ([]happydns.Record, error) {
+	return s.ListRecordsUC.List(domain, zone)
+}
+
+func (s *Service) GetZone(zoneid happydns.Identifier) (*happydns.Zone, error) {
+	return s.GetZoneUC.Get(zoneid)
+}
+
+func (s *Service) GetZoneMeta(zoneid happydns.Identifier) (*happydns.ZoneMeta, error) {
+	return s.GetZoneUC.GetMeta(zoneid)
+}
+
+func (s *Service) LoadZoneFromId(domain *happydns.Domain, id happydns.Identifier) (*happydns.Zone, error) {
+	return s.GetZoneUC.GetInDomain(id, domain)
+}
+
+func (s *Service) UpdateZone(zoneID happydns.Identifier, updateFn func(*happydns.Zone)) error {
+	return s.UpdateZoneUC.Update(zoneID, updateFn)
+}

commit 72fa2b3904af04fafcb58319e4529637d4f3fca5

fix: wire ActionOnEditableZone into all zone mutation facades
Add/update/delete service calls in the Service facade were bypassing
ActionOnEditableZone, so mutations could silently target a committed or
published zone instead of deriving a new editable snapshot first.

Wraps AddServiceToZone, RemoveServiceFromZone, and UpdateZoneService
with ActionOnEditableZone so the decorator is applied consistently.

Fixes regression introduced by b2b6467575c5e551e76f72205469b3f185291fe2.

internal/usecase/zone_service/action_on_zone.go
@@ -72,7 +72,7 @@ func (uc *ActionOnDomainUsecase) ActionOnEditableZone(
 		err = uc.zoneCreator.Create(newZone)
 		if err != nil {
 			return nil, happydns.InternalError{
-				Err:         fmt.Errorf("unable to CreateZone in importZone: %s\n", err),
+				Err:         fmt.Errorf("unable to CreateZone in importZone: %s", err),
 				UserMessage: "Sorry, we are unable to create your zone.",
 			}
 		}
@@ -85,7 +85,7 @@ func (uc *ActionOnDomainUsecase) ActionOnEditableZone(
 		})
 		if err != nil {
 			return nil, happydns.InternalError{
-				Err:         fmt.Errorf("unable to UpdateDomain in importZone: %s\n", err),
+				Err:         fmt.Errorf("unable to UpdateDomain in importZone: %s", err),
 				UserMessage: "Sorry, we are unable to create your zone.",
 			}
 		}

internal/usecase/zone_service/factory.go
@@ -73,7 +73,7 @@ func (s *Service) ActionOnEditableZone(
 	return s.ActionOnDomainUC.ActionOnEditableZone(user, domain, zone, act)
 }
 
-// AddServiceToZone delegates to AddToZoneUsecase.AddService.
+// AddServiceToZone ensures the zone is editable then delegates to AddToZoneUsecase.AddService.
 func (s *Service) AddServiceToZone(
 	user *happydns.User,
 	domain *happydns.Domain,
@@ -82,10 +82,12 @@ func (s *Service) AddServiceToZone(
 	origin happydns.Origin,
 	service *happydns.Service,
 ) (*happydns.Zone, error) {
-	return zone, s.AddToZoneUC.AddService(zone, subdomain, origin, service)
+	return s.ActionOnDomainUC.ActionOnEditableZone(user, domain, zone, func(z *happydns.Zone) error {
+		return s.AddToZoneUC.AddService(z, subdomain, origin, service)
+	})
 }
 
-// RemoveServiceFromZone delegates to DeleteFromZoneUsecase.DeleteService.
+// RemoveServiceFromZone ensures the zone is editable then delegates to DeleteFromZoneUsecase.DeleteService.
 func (s *Service) RemoveServiceFromZone(
 	user *happydns.User,
 	domain *happydns.Domain,
@@ -93,10 +95,12 @@ func (s *Service) RemoveServiceFromZone(
 	subdomain happydns.Subdomain,
 	serviceID happydns.Identifier,
 ) (*happydns.Zone, error) {
-	return zone, s.DeleteFromZoneUC.DeleteService(zone, subdomain, serviceID)
+	return s.ActionOnDomainUC.ActionOnEditableZone(user, domain, zone, func(z *happydns.Zone) error {
+		return s.DeleteFromZoneUC.DeleteService(z, subdomain, serviceID)
+	})
 }
 
-// UpdateZoneService delegates to UpdateServiceUsecase.Update.
+// UpdateZoneService ensures the zone is editable then delegates to UpdateServiceUsecase.Update.
 func (s *Service) UpdateZoneService(
 	user *happydns.User,
 	domain *happydns.Domain,
@@ -105,5 +109,7 @@ func (s *Service) UpdateZoneService(
 	serviceID happydns.Identifier,
 	service *happydns.Service,
 ) (*happydns.Zone, error) {
-	return zone, s.UpdateServiceUC.Update(zone, subdomain, serviceID, service)
+	return s.ActionOnDomainUC.ActionOnEditableZone(user, domain, zone, func(z *happydns.Zone) error {
+		return s.UpdateServiceUC.Update(z, subdomain, serviceID, service)
+	})
 }

web/src/routes/domains/[dn]/[[historyid]]/[subdomain]/[serviceid]/+page.svelte
@@ -41,6 +41,7 @@
     import { thisZone } from "$lib/stores/thiszone";
     import { navigate } from "$lib/stores/config";
     import { t } from "$lib/translations";
+    import { refreshDomains } from "$lib/stores/domains";
 
     interface Props {
         data: {
@@ -93,9 +94,9 @@
 
     let addServiceInProgress = $state(false);
 
-    function goBack() {
+    function goBack(historyid?: string) {
         navigate(
-            `/domains/${encodeURIComponent(data.domain.domain)}/${encodeURIComponent(data.history)}`,
+            `/domains/${encodeURIComponent(data.domain.domain)}/${encodeURIComponent(historyid ? historyid : data.history)}`,
         );
     }
 
@@ -110,7 +111,13 @@
             (z) => {
                 thisZone.set(z);
                 addServiceInProgress = false;
-                goBack();
+                if (service?._id) {
+                    goBack();
+                } else {
+                    refreshDomains().then(() => {
+                        goBack(z.id);
+                    });
+                }
             },
             (err) => {
                 addServiceInProgress = false;